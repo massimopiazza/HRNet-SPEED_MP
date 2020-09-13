9ee7d3b2bd3c76d0ba7874359941c0406238116f
massimopiazza <massimopiazza97@gmail.com>
2020-09-13 11:02:41 +0200

    Initial commit

--- tools/MAIN_inference.py
+++ tools/MAIN_inference.py
@@ -26,10 +26,9 @@ import models
 
 ##
 
-
 # logger, final_output_dir, tb_log_dir = create_logger(
-    cfg, args.cfg, 'valid')
-
+#     cfg, args.cfg, 'valid')
+#
 # logger.info(pprint.pformat(args))
 # logger.info(cfg)
 
@@ -41,3 +40,21 @@ torch.backends.cudnn.enabled = cfg.CUDNN.ENABLED
 model = eval('models.' + cfg.MODEL.NAME + '.get_pose_net')(
     cfg, is_train=False
 )
+
+# load weights
+if cfg.TEST.MODEL_FILE:
+    logger.info('=> loading model from {}'.format(cfg.TEST.MODEL_FILE))
+    model.load_state_dict(torch.load(cfg.TEST.MODEL_FILE), strict=False)
+else:
+    model_state_file = os.path.join(
+        final_output_dir, 'final_state.pth'
+    )
+    logger.info('=> loading model from {}'.format(model_state_file))
+    model.load_state_dict(torch.load(model_state_file))
+
+model = torch.nn.DataParallel(model, device_ids=cfg.GPUS).cuda()
+
+# define loss function (criterion) and optimizer
+criterion = JointsMSELoss(
+    use_target_weight=cfg.LOSS.USE_TARGET_WEIGHT
+).cuda()
