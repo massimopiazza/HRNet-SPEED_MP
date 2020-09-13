85f9f2e72516a3a9ef6b8249a5e2e2db0d560f86
massimopiazza <massimopiazza97@gmail.com>
2020-09-13 09:52:45 +0200

    Initial commit

---
+++ MAIN_inference.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import pprint
+
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+
+import _init_paths
+from config import cfg
+from config import update_config
+from core.loss import JointsMSELoss
+from core.function import validate
+from utils.utils import create_logger
+
+import dataset
+import models
+
+
+
+##
+
+
+logger, final_output_dir, tb_log_dir = create_logger(
+    cfg, args.cfg, 'valid')
+
+logger.info(pprint.pformat(args))
+logger.info(cfg)
+
+# cudnn related setting
+cudnn.benchmark = cfg.CUDNN.BENCHMARK
+torch.backends.cudnn.deterministic = cfg.CUDNN.DETERMINISTIC
+torch.backends.cudnn.enabled = cfg.CUDNN.ENABLED
+
+model = eval('models.' + cfg.MODEL.NAME + '.get_pose_net')(
+    cfg, is_train=False
+)
